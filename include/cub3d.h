495aca79ca925603a3308b91e0e577f87b1582da
Mehrin Firdousi <mfirdous@lab3r8s9.42abudhabi.ae>
2023-04-27 19:55:35 +0400

    Empty window done

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: mfirdous <mfirdous@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 20:22:27 by mfirdous          #+#    #+#             */
-/*   Updated: 2023/04/26 20:27:20 by mfirdous         ###   ########.fr       */
+/*   Updated: 2023/04/27 19:31:46 by mfirdous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,20 @@
 # include "libft.h"
 # include <mlx.h>
 # include <math.h>
+
+typedef struct s_player
+{
+	int		x;
+	int		y;
+	char	orientation;
+}	t_player;
+
+typedef struct s_map
+{
+	int		ceil_color;
+	int		floor_color;
+	// texture paths - 4
+	char	**map;
+}	t_map;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mfirdous <mfirdous@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 20:25:33 by mfirdous          #+#    #+#             */
-/*   Updated: 2023/04/26 20:30:45 by mfirdous         ###   ########.fr       */
+/*   Updated: 2023/04/27 19:55:14 by mfirdous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,15 @@
 
 int	main(int argc, char **argv)
 {
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello");
+	mlx_loop(mlx);
+
 	(void)argc;
 	(void)argv;
+
 	return (0);
 }
